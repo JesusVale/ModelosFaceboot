fa1f89ca07ab7ec69818e6352326c80a626f9853
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2022-10-29 17:02:32 -0700

    Creación del modelo usuario
    modelo usuario semi hecho

---
+++ Modelo/src/main/java/modelo/ClienteModelo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author tonyd
+ */
+public class ClienteModelo {
+    int puerto;
+    Socket sc;
+    BufferedReader in;
+    BufferedWriter out;
+    
+    public void enviarMensaje(String mensaje){
+    }
+}
