5763f19acdabdd34bac8850c802ef419b31a37a9
Antonio-dp <antonio.delpardo229161@potros.itson.edu.mx>
2022-10-29 23:31:17 -0700

    Nombrar base de datos

--- Modelo/src/main/java/modelo/ConexionBD.java
+++ Modelo/src/main/java/modelo/ConexionBD.java
@@ -17,7 +17,7 @@ public class ConexionBD implements IConexionBD{
     @Override
     public EntityManager crearConexion() throws IllegalStateException {
         //Obtiene acceso alemFactory a partir de la persistence unit utilizada
-        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("puntoVenta_DominioPU"); 
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("facebootPU"); 
         // Creamos una em(bd) para realizar operaciones con la bd
         EntityManager em = emFactory.createEntityManager();
         return em;
